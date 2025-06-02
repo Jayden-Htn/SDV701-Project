e67bb5b173a81097eae5c9fdfd700bd890645448
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2025-06-02 17:58:58 +1200

    Improve display of test products

--- reactapp/src/components/products.tsx
+++ reactapp/src/components/products.tsx
@@ -6,11 +6,11 @@ const Products = () => {
   const [content, setContent] = useState<object>();
 
   useEffect(() => {
-    setContent([
+    var content = [
       {
         id: 1,
         name: "Name",
-        description: "Description",
+        description: "Description description description description description description description description description description",
         price: 13.55,
         quantityAvailable: 5,
         brand: "Tooler",
@@ -19,13 +19,14 @@ const Products = () => {
       {
         id: 2,
         name: "Name2",
-        description: "Description2",
+        description: "Description description description description description description description description description description",
         price: 11.55,
         quantityAvailable: 10,
-        brand: "Tooler",
+        brand: "MowGo",
         type: "RideOn"
       }
-    ])
+    ]
+    setContent([...content, ...content, ...content, ...content]);
   }, []);
   
 
@@ -37,12 +38,22 @@ const Products = () => {
           Array.isArray(content) ? (
             content.map((product, i) => (
               <div key={i} className={styles.itemCard}>
-                <Link to={`/Product/${product?.id}`}>
-                  <h3>{product.name}</h3>
-                </Link>
-                <p>{product.description}</p>
-                <p>Price: {product.price}</p>
-                <p>Quantity: {product.quantityAvailable}</p>
+                <img src={product.photo} className={styles.photo}></img>
+                <div className={styles.cardText}>
+                  <Link to={`/Product/${product?.id}`}>
+                    <h3 className={styles.textItem}>{product.brand} {product.name}</h3>
+                  </Link>
+                  <div className={styles.textRow}>
+                    <p className={styles.textItem}><b>Price: ${product.price}</b></p>
+                    <p className={styles.textItem}>Quantity: {product.quantityAvailable}</p>
+                  </div>
+                  <p className={styles.textItem}>{product.description}</p>
+                  <div className={styles.textRow}>
+                    <p className={styles.textItem}>Category: {product.type}</p>
+                    <p className={styles.textItem}>Brand: {product.brand}</p>
+                  </div>
+                  
+                </div>
               </div>
             ))
           ) : (
